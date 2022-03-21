9f7d9f7ef0ae50e69de47fcbade8a9f5a2260efc
Lucas Ezequiel Rossini <rossiniluca25@gmail.com>
2022-03-21 12:21:55 -0300

    corrigiendo errores ortográficos

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@ const prod1 = 10;
 const prod2 = 20;
 const prod3 = 30;
 const prod4 = 40;
-let prod = parseInt(prompt('Ingrese el número correspondiente al producto que desea comprar: \n\n 1- Producto 1: $10 \n 2- Producto 2: $20 \n 3- Producto 3: $30 \n 4- Producto 4: $40 \n 0- Ninguno'));
+let prod = parseInt(prompt('Ingrese el número correspondiente al producto que desea comprar: \n\n 1- Producto 1: $10 \n 2- Producto 2: $20 \n 3- Producto 3: $30 \n 4- Producto 4: $40 \n 0- Ninguno (finaliza el programa)'));
 let lista = "";
 let suma = 0;
 
-//! Función para agregar pruducto a la lsita
+//! Función para agregar pruducto a la lista
 function list(prod) {
     switch (prod) {
     case 1: 
@@ -28,7 +28,7 @@ function list(prod) {
      return lista;
  };
 
-//! Función para sumar el producto elegido y sumarlo a la lsita
+//! Función para sumar precio del producto elegido
 function carrito(prod) {
    switch (prod) {
     case 1: 
@@ -55,14 +55,14 @@ if (prod == 0) {
 } else {
     //! Ciclo while para agregar productos al carrito
     while ((prod != 0)) {
-        //! Condicional para restringir ingresos a opciones válidas
+        //! Condicional para limitar ingresos a opciones válidas
         if (prod >=5) {
             prod = parseInt(prompt('No ingresó una opción válida. Vuelva a ingresar un número'));
         } else {
             suma = carrito(prod);
             lista = list(prod);
             alert('Los productos agregados hasta ahora son:' + lista + '\n\nY el total hasta ahora es de $' + suma + '.');
-            prod = parseInt(prompt('Ingrese el número correspondiente al producto que desea comprar: \n\n 1- Producto 1: $10 \n 2- Producto 2: $20 \n 3- Producto 3: $30 \n 4- Producto 4: $40 \n 0- Ninguno'));
+            prod = parseInt(prompt('Ingrese el número correspondiente al producto que desea comprar: \n\n 1- Producto 1: $10 \n 2- Producto 2: $20 \n 3- Producto 3: $30 \n 4- Producto 4: $40 \n 0- Ninguno (finaliza el programa)'));
         };
     };
     alert('La lista final de productos es: ' + lista + '\n\nY el total final es de $' + suma + '\n\nFin del programa.');
